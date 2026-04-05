db003b03689409b423b97a8b6d6afeef14fe587b
Pooji07-29 <pgandeti@student.gitam.edu>
2026-04-06 01:10:39 +1200

    Created shortestpath.c

--- shortestpath.c
+++ shortestpath.c
@@ -14,7 +14,6 @@ int main() {
         for (int j = 0; j < n; j++)
             scanf("%d", &dist[i][j]);
 
-    // Floyd-Warshall
     for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)

6a55120a79b48a85f4a72430cfa14bd6703638aa
Pooji07-29 <pgandeti@student.gitam.edu>
2026-04-06 01:08:41 +1200

    Created shortestpath.c

---
+++ shortestpath.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define INF 99999
+
+int main() {
+    int n;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &dist[i][j]);
+
+    // Floyd-Warshall
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
+    printf("\nShortest distance matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
